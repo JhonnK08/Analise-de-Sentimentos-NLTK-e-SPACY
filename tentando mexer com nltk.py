ee7d4eb6178562a59e1c3d63ef244f6dbbec9a0c
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2020-05-10 20:04:30 -0300

    nltk
    Nao sei se esta funcionando

--- tentando mexer com nltk.py
+++ tentando mexer com nltk.py
@@ -14,6 +14,57 @@ from nltk.corpus import brown
 from nltk.stem import WordNetLemmatizer
 
 
+dicionario = {}
+lemmas = WordNetLemmatizer()
+
+
+with open('DICIONÁRIOS\oplexicon_v3.0\oplexicon.csv', encoding='utf8') as csvfile:
+     lines = csv.reader(csvfile, delimiter=';')
+     oplexico = list(lines)
+     for i in range(len(oplexico)):
+         if i > 0:
+             palavra = oplexico[i][0]
+             polaridade = oplexico[i][1]
+             print(palavra, polaridade)
+             dicionario[palavra] = polaridade
+
+
+
+with open('DICIONÁRIOS\lexiconPT-master\data-raw\lexicon.csv', encoding='utf8') as csvfile:
+    lines = csv.reader(csvfile, delimiter=';')
+    lexicon = list(lines)
+    for i in range(len(lexicon)):
+        if i > 0:
+            palavra = lexicon[i][0]
+            polaridade = lexicon[i][1]
+            print(palavra, polaridade)
+            dicionario[palavra] = polaridade
+
+
+
+with open('DICIONÁRIOS\wordNetAffect\wordnet.csv') as csvfile:
+    lines = csv.reader(csvfile, delimiter=';')
+    wordnet = list(lines)
+    for i in range(len(wordnet)):
+        if i > 0:
+            palavra = wordnet[i][0]
+            polaridade = wordnet[i][1]
+            print(palavra, polaridade)
+            dicionario[palavra] = polaridade
+
+
+
+with open('DICIONÁRIOS\senticnet-1.3\senticnet_pt.csv', encoding='utf8') as csvfile:
+    lines = csv.reader(csvfile, delimiter=';')
+    senticnet = list(lines)
+    for i in range(len(senticnet)):
+        if i > 0:
+            palavra = senticnet[i][0]
+            polaridade = senticnet[i][1]
+            #print(palavra, polaridade)
+            dicionario[palavra] = polaridade
+
+
 
 #with open('ANALISE DOS DADOS\CHATS\Chat_ATIV_01_04\Chat_ATIV_01_04.csv','r') as csv_file:
 #        csv_reader = csv.reader(csv_file, delimiter=';')
@@ -40,6 +91,8 @@ stemmer = nltk.stem.RSLPStemmer()
 #tokens = word_tokenize(sentenca)
 #tags = pos_tag(tokens)
 
+
+
 #Pré-processamento
 def Preprocessamento(texto) :
     texto = str(texto)
@@ -49,14 +102,46 @@ def Preprocessamento(texto) :
     lista = []
     #Tokenizacao
     for token in documento:
-        lista.append(stemmer.stem(token))
+        lista.append(lemmas.lemmatize(token))
     lista = [palavra for palavra in lista if palavra not in stopwords and palavra not in pontuacao] # retira stopwords
     lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()]) # retira digitos
-
     return lista
 
 
+#POLARIDADE DA PALAVRA
+def Polaridade (frase):
+    frasepolaridade = []
+    for i in frase:
+        print(i)
+        frasepolaridade.append(float(dicionario.get(i, 0)))
+        #print(frasepolaridade)
+    score = sum(frasepolaridade)
+    return score
 
+#ANALISE DE SENTIMENTO
+def analises(frase):
+    print('Frase: ' + frase)
+    fraseprocessada = Preprocessamento(frase)
+    fraseprocessada2 = ' '.join([str(elemento) for elemento in fraseprocessada])
+    scorefrase = Polaridade(fraseprocessada)
+    #print('Score da frase: ' + str(scorefrase))
+    if scorefrase > 0:
+        print('Positiva')
+    elif scorefrase == 0:
+        print('Neutra')
+    elif scorefrase < 0:
+        print('Negativo')
+    print(fraseprocessada2 + ' , ' + str(scorefrase))
+    return scorefrase
+
+scores = []
+scores = chat0104['MENSAGEM'].apply(analises)
+chat0104dic = pandas.DataFrame(chat0104)
+chat0104dic['POLARIDADE'] = scores
+chat0104dic.to_csv(r'ANALISE DOS DADOS\CHATS\Chat_ATIV_01_04\Chat_ATIV_01_04-analise.csv', index = False)
+
+
+"""
 chat0104['MENSAGEM'] = chat0104['MENSAGEM'].apply(Preprocessamento)
 chat0104.to_csv(r'ANALISE DOS DADOS\CHATS\Chat_ATIV_01_04\Chat_ATIV_01_04-stem.csv', index = False)
 chat0107['MENSAGEM'] = chat0107['MENSAGEM'].apply(Preprocessamento)
@@ -65,3 +150,4 @@ chat0507['MENSAGEM'] = chat0507['MENSAGEM'].apply(Preprocessamento)
 chat0507.to_csv(r'ANALISE DOS DADOS\CHATS\Chat_ATIV_05_07\Chat_ATIV_05_07-stem.csv', index = False)
 chat0810['MENSAGEM'] = chat0810['MENSAGEM'].apply(Preprocessamento)
 chat0810.to_csv(r'ANALISE DOS DADOS\CHATS\Chat_ATIV_08_10\Chat_ATIV_08_10-stem.csv', index = False)
+"""
